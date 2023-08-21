c6dafc6fcb45b7e4270dbc3d91563a08f29be544
Ridrox93 <popovandrii@yahoo.com>
2023-08-21 20:25:02 +0200

    next

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const textInputElement = document.querySelector("#validation-input");
 
 
 textInputElement.addEventListener("input", () => {
-     if (textInputElement.getAttribute('data-length') < textInputElement.value.length) {
+     if (textInputElement.getAttribute('data-length') === textInputElement.value.length) {
     textInputElement.classList.add("valid");
     textInputElement.classList.remove("invalid");
   } else {
